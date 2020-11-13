7f88baa9a0206af49249ba5df5eb48dc62964b1c
范轶洁 <fanyjb@digitalchina.com>
2020-11-13 16:50:59 +0800

    fix wx:for issue

--- pages/index/index.js
+++ pages/index/index.js
@@ -24,40 +24,40 @@ create.Page(store, {
     })
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.store.data.userInfo = app.globalData.userInfo
-      this.store.data.hasUserInfo = true
+    // if (app.globalData.userInfo) {
+    //   this.store.data.userInfo = app.globalData.userInfo
+    //   this.store.data.hasUserInfo = true
 
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.store.data.userInfo = res.userInfo
-        this.store.data.hasUserInfo = true
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.store.data.userInfo = res.userInfo
-          this.store.data.hasUserInfo = true
-        }
-      })
-    }
+    // } else if (this.data.canIUse) {
+    //   // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+    //   // 所以此处加入 callback 以防止这种情况
+    //   app.userInfoReadyCallback = res => {
+    //     this.store.data.userInfo = res.userInfo
+    //     this.store.data.hasUserInfo = true
+    //   }
+    // } else {
+    //   // 在没有 open-type=getUserInfo 版本的兼容处理
+    //   wx.getUserInfo({
+    //     success: res => {
+    //       app.globalData.userInfo = res.userInfo
+    //       this.store.data.userInfo = res.userInfo
+    //       this.store.data.hasUserInfo = true
+    //     }
+    //   })
+    // }
 
-    setTimeout(() => {
-      this.store.data.logs.push('abc')
-      this.store.data.motto = '123456'
-    }, 1000)
+    // setTimeout(() => {
+    //   this.store.data.logs.push('abc')
+    //   this.store.data.motto = '123456'
+    // }, 1000)
 
-    setTimeout(() => {
-      this.store.data.motto = 'abcdefg'
-    }, 2000)
+    // setTimeout(() => {
+    //   this.store.data.motto = 'abcdefg'
+    // }, 2000)
 
-    setTimeout(() => {
-      this.store.set(this.store.data, 'newProp', 'newPropVal')
-    }, 3000)
+    // setTimeout(() => {
+    //   this.store.set(this.store.data, 'newProp', 'newPropVal')
+    // }, 3000)
 
 
     const handler = function (evt) {
